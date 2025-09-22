646276fc83b1a3ceb9698ac5e1e0442792812d70
Laura Sofía Agredo <tucorreo@ejemplo.com>
2025-09-21 23:12:32 -0500

    cambio

--- ejer2.js
+++ ejer2.js
@@ -1,7 +1,7 @@
 // Cambiar el href de un enlace usando getElementsByTagName
 const enlaces = document.getElementsByTagName("a");
 if (enlaces.length > 0) {
-  enlaces[0].href = "https://www.google.com/?hl=es&zx=1758512946628&no_sw_cr=1";
+  enlaces[0].href = "https://www.google.com";
   enlaces[0].textContent = "Ir a Google";
 }
 
