29b3a1e55d3dbad61be6d2c0cbc046a19921801c
Laura Sofía Agredo <tucorreo@ejemplo.com>
2025-09-21 23:22:41 -0500

    taller don

--- ejer2.js
+++ ejer2.js
@@ -1,13 +1,13 @@
-// Cambiar el href de un enlace usando getElementsByTagName
 const enlaces = document.getElementsByTagName("a");
 if (enlaces.length > 0) {
-  enlaces[0].href = "https://www.google.com";
-  enlaces[0].textContent = "Ir a Google";
+  enlaces[0].href = "https://www.google.com";      
+  enlaces[0].textContent = "Ir a Google";          
+  enlaces[0].setAttribute("target", "_blank");    
 }
 
-// Cambiar contenido de la clase titulo
+
 const titulo = document.querySelector(".titulo");
 titulo.textContent = "Título Modificado";
 
-// Cambiar color de fondo del body
+
 document.body.style.background = "#D6D6D6FF";
